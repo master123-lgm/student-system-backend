a1beeed4656b911a6ba002689ece8a139ba457c4
Brian Mringo <brianmringo2@gmail.com>
2026-03-28 18:15:15 +0300

    Prepare backend for deployment

--- src/main/java/com/example/studentsystembackend/student/StudentController.java
+++ src/main/java/com/example/studentsystembackend/student/StudentController.java
@@ -5,7 +5,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import java.util.List;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,7 +18,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/students")
-@CrossOrigin(origins = "http://localhost:4200")
 public class StudentController {
 
   private final StudentService studentService;
